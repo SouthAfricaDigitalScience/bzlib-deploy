b7e7f2c261de94162eef347f6ca75ab3f6ae8578
Bruce Becker <brucellino@gmail.com>
2015-12-09 16:16:54 +0100

    missing variable

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # boost has a different naming convention to most, for it's source tarballs. Instead of using x.y.z it uses x_y_z
 # We have to change the name of the tarbal then, later
 module add ci
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
 mkdir -p ${WORKSPACE}
 mkdir -p ${SOFT_DIR}
 mkdir -p ${SRC_DIR}
@@ -11,7 +12,7 @@ mkdir -p ${SRC_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "tarball's not here ! let's get it"
-  wget  http://www.bzip.org/${VERSION}/${NAME}-${VERSION}.tar.gz -O ${SRC_DIR}/${SOURCE_FILE}
+  wget  http://www.bzip.org/${VERSION}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
