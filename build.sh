03ff1dcca279b496174e20b3e35dc2d11472f349
Bruce Becker <brucellino@gmail.com>
2015-12-09 16:13:35 +0100

    First Commit of bzlib

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+# boost has a different naming convention to most, for it's source tarballs. Instead of using x.y.z it uses x_y_z
+# We have to change the name of the tarbal then, later
+module add ci
+mkdir -p ${WORKSPACE}
+mkdir -p ${SOFT_DIR}
+mkdir -p ${SRC_DIR}
+
+
+if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
+  touch  ${SRC_DIR}/${SOURCE_FILE}.lock
+  echo "tarball's not here ! let's get it"
+  wget  http://www.bzip.org/${VERSION}/${NAME}-${VERSION}.tar.gz -O ${SRC_DIR}/${SOURCE_FILE}
+  echo "releasing lock"
+  rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
+elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
+  # Someone else has the file, wait till it's released
+  while [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; do
+    echo " There seems to be a download currently under way, will check again in 5 sec"
+    sleep 5
+  done
+else
+  echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
+fi
+tar xzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
+cd ${WORKSPACE}/${NAME}-${VERSION}
+./configure --prefix=${SOFT_DIR}
+make
---
+++ check-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+module add ci
+cd ${WORKSPACE}/${NAME}-${VERSION}
+make install
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+  puts stderr "\\tAdds $NAME ($VERSION.) to your environment."
+}
+module-whatis "Sets the environment for using $NAME ($VERSION.) See https://github.com/SouthAfricaDigitalScience/bzlib-deploy"
+setenv BZLIB_VERSION $VERSION
+set BZLIB_DIR /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path CPATH ${BZLIB_DIR}/include
+prepend-path LD_LIBRARY_PATH ${BZLIB_DIR}/lib
+prepend-path LD_LIBRARY_PATH ${BZLIB_DIR}/lib64
+MODULE_FILE
+) > modules/${VERSION}
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+cp modules/${VERSION} ${LIBRARIES_MODULES}/${NAME}
+module avail
+module add ${NAME}
+which bzip2
